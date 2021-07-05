5ca26ae57df90d1f72e31917dabdc72bb2659979
bekir geriş <bekir.geris@gmail.com>
2021-07-05 15:21:13 +0300

    05/07/2021 15:20

--- app/src/main/java/com/begers/dongel/AnaSayfa.java
+++ app/src/main/java/com/begers/dongel/AnaSayfa.java
@@ -2,6 +2,7 @@ package com.begers.dongel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,22 +37,34 @@ public class AnaSayfa extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton rb = findViewById(checkedId);
-                numberOfPlayer = Integer.parseInt(rb.getText().toString());
+                numberOfPlayer = Integer.parseInt(rb.getText().toString().replace(" ",""));
+                textViewPlayers.setText("Oyuncu Listesi (" + numberOfPlayer + ")");
+                players.clear();
             }
         });
 
         butonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                players.add(new Players(editNames.getText().toString(), 0));
+
+                if (!editNames.getText().toString().replace(" ", "").equals("")){
+                    players.add(new Players(editNames.getText().toString(), 0));
+                }else {
+                    Toast.makeText(getApplicationContext(), "Oyuncu Adı Boş Olamaz", Toast.LENGTH_LONG).show();
+                }
                 editNames.setText("");
 
-                textViewPlayers.setText("");
+                textViewPlayers.setText("Oyuncu Listesi (" + numberOfPlayer + ")");
                 for (Players player : players){
-                    textViewPlayers.setText(textViewPlayers.getText()  + "" + player.getPlayerName() + "\n");
+                    textViewPlayers.setText(textViewPlayers.getText()  + "\n" + player.getPlayerName());
+                }
+
+                if (players.size() == numberOfPlayer){
+                    //ikinci sayfaya git
                 }
             }
         });
+
     }
 
     public void tanimlamalar(){
