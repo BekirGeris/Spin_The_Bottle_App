59935cb926798f76897c1a856778dd55cf24699b
bekir geriş <bekir.geris@gmail.com>
2021-07-06 13:03:52 +0300

    06/07/2021 13:03

--- app/src/main/java/com/begers/dongel/AnaSayfa.java
+++ app/src/main/java/com/begers/dongel/AnaSayfa.java
@@ -7,21 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AnaSayfa extends AppCompatActivity {
 
+    private AdView mAdView;
+
     private int numberOfPlayer;
 
     private RadioGroup radioGroup;
     private EditText editNames;
-    private Button butonAdd;
+    private ImageButton butonAdd;
     private TextView textViewPlayers;
 
     @Override
@@ -32,14 +39,16 @@ public class AnaSayfa extends AppCompatActivity {
         List<Players> players = new ArrayList<>();
 
         tanimlamalar();
+        bannerReklam();
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton rb = findViewById(checkedId);
                 numberOfPlayer = Integer.parseInt(rb.getText().toString().replace(" ",""));
-                textViewPlayers.setText("Oyuncu Listesi (" + numberOfPlayer + ")");
+                textViewPlayers.setText(getString(R.string.liste) + " (" + numberOfPlayer + ")");
                 players.clear();
+                butonAdd.setImageResource(R.drawable.oyuncu_add);
             }
         });
 
@@ -50,13 +59,17 @@ public class AnaSayfa extends AppCompatActivity {
                 if (!editNames.getText().toString().replace(" ", "").equals("")){
                     players.add(new Players(editNames.getText().toString(), 0));
                 }else {
-                    Toast.makeText(getApplicationContext(), "Oyuncu Adı Boş Olamaz", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), getString(R.string.toolsMessage_1), Toast.LENGTH_LONG).show();
                 }
                 editNames.setText("");
 
-                textViewPlayers.setText("Oyuncu Listesi (" + numberOfPlayer + ")");
+                textViewPlayers.setText(getString(R.string.liste) + " ("  + numberOfPlayer + ")");
                 for (Players player : players){
                     textViewPlayers.setText(textViewPlayers.getText()  + "\n" + player.getPlayerName());
+                }
+
+                if (players.size() == numberOfPlayer - 1){
+                    butonAdd.setImageResource(R.drawable.ikinci_sayfaya_git);
                 }
 
                 if (players.size() == numberOfPlayer){
@@ -68,10 +81,16 @@ public class AnaSayfa extends AppCompatActivity {
     }
 
     public void tanimlamalar(){
+        MobileAds.initialize(this, getString(R.string.appId));
         radioGroup = findViewById(R.id.radio_grup);
         editNames = findViewById(R.id.edit_names);
         butonAdd = findViewById(R.id.buton_add);
         textViewPlayers = findViewById(R.id.text_players);
     }
 
+    private void bannerReklam() {
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
 }
